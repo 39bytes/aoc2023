926414335ae572f5c4af8ecd62c1271a3909edde
JeffDotPng <zhangjeff2014@gmail.com>
2023-12-01 10:50:37 -0500

    slightly cleaner day 1

--- day1/day1.hs
+++ day1/day1.hs
@@ -8,17 +8,17 @@ digitLine s = read [firstDigit s, firstDigit (reverse s)]
   where
     firstDigit s = head $ filter isDigit s
 
-digits :: [(String, Char)]
-digits = [("one", '1'), ("two", '2'), ("three", '3'), ("four", '4'), ("five", '5'), ("six", '6'), ("seven", '7'), ("eight", '8'), ("nine", '9')]
+digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 digitLine2 :: String -> Int
 digitLine2 s = read [firstDigit2 s, lastDigit2 s]
   where
-    firstDigit2 s = case find ((`isPrefixOf` s) . fst) digits of
+    ds = zip digits ['1' .. '9']
+    firstDigit2 s = case find ((`isPrefixOf` s) . fst) ds of
       Just (_, n) -> n
       Nothing -> let c = head s in if isDigit c then c else firstDigit2 (tail s)
 
-    lastDigit2 s = case find ((`isSuffixOf` s) . fst) digits of
+    lastDigit2 s = case find ((`isSuffixOf` s) . fst) ds of
       Just (_, n) -> n
       Nothing -> let c = last s in if isDigit c then c else lastDigit2 (init s)
 
