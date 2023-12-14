022d54f2ee440d59b57040fc129a9b22fb1ded74
JeffDotPng <zhangjeff2014@gmail.com>
2023-12-14 00:30:26 -0500

    better day 12 part 1

--- day12/day12.hs
+++ day12/day12.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List qualified as L
 import Data.List.Split (splitOn)
 import Debug.Trace (trace)
 
@@ -12,34 +13,39 @@ parse = map parseRecord
       let [springs, groupLengths] = words l
        in (springs, map read $ splitOn "," groupLengths)
 
+unfoldRecord :: Record -> Record
+unfoldRecord (springs, groups) = (springs', groups')
+  where
+    springs' = L.intercalate "?" $ replicate 5 springs
+    groups' = concat $ replicate 5 groups
+
 debug = flip trace
 
 arrangements :: Record -> Int
-arrangements (springs, groups) = dfs springs groups 0
+arrangements (springs, groups) = dfs springs groups
 
--- Brute force
-dfs :: String -> [Int] -> Int -> Int
-dfs "" [] _ = 1
-dfs "" [g] groupCount | g == groupCount = 1
-dfs cs [] _ | all (`elem` ".?") cs = 1
-dfs cs gs _ | null cs || null gs = 0
-dfs (c : cs) groups@(g : gs) groupCount = case c of
+dfs :: String -> [Int] -> Int
+dfs "" [] = 1
+dfs cs [] | '#' `notElem` cs = 1
+dfs cs gs | null cs || null gs = 0
+dfs (c : cs) groups@(g : gs) = case c of
   '?' -> pickPath '.' + pickPath '#'
   c -> pickPath c
   where
+    (group, rest) = splitAt (g - 1) cs
+    validGroup = '.' `notElem` group && length group == g - 1
     pickPath c = case c of
-      '.' | groupCount == g -> dfs cs gs 0
-      '.' | groupCount == 0 -> dfs cs groups 0
-      '.' -> 0
-      '#' | groupCount >= g -> 0
-      '#' -> dfs cs groups (groupCount + 1)
+      '.' -> dfs cs groups
+      '#' | validGroup && null rest -> dfs rest gs
+      '#' | validGroup && head rest /= '#' -> dfs (tail rest) gs
+      '#' -> 0
 
-solve1 :: [Record] -> Int
-solve1 rs = sum $ map arrangements rs
+solve :: [Record] -> Int
+solve rs = sum $ map arrangements rs
 
 main :: IO ()
 main = do
   inp <- getContents
   let ls = lines inp
   let records = parse ls
-  print $ solve1 records
+  print $ solve records
